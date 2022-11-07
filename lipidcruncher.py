e11fe56c5d0ab0a892071f4c3844f1668d1bb735
Farese & Walther Lab <86812170+FW-lab@users.noreply.github.com>
2022-11-07 14:29:37 -0500

    Update lipidcruncher.py

--- lipidcruncher.py
+++ lipidcruncher.py
@@ -6,12 +6,21 @@ from apps import lipidsearch4, lipidsearch5, lipidxplorer # import modules
 app = MultiApp()
 
 st.markdown("""
-# LipidCruncher
-
+# LipidCruncher: a web application for performing general lipidomics 
+One of our missions at Farese/Walther lab is making transformative discoveries in lipid metabolism. 
+We want to understand how our bodies store and channel energy. We are hypothesis driven. 
+A typical study in our lab is as follows: the lab biologists come up with a hypothesis (driven by deep expertise and intuition built over many years),
+then they design/run an experiment to test the hypothesis. 
+They usually have a control group where nothing is changed and one or more experimental groups where something is changed 
+(i.g. a certain gene is knocked out or is over expressed). They prepare multiple replicates for each group and run them though mass spectrometer. 
+One of the things that they are interested in is whether there is a significant difference in the lipid profile between 
+the control group and the experimental group(s).
 In the context of lipidomics, the output of mass spectrometry is the relative abundance of the lipid species that make up the sample under study. 
-This output is in the form of a spectrum in which the peaks represent an identified lipid species and the area underneath each peak reperesnts the relative 
-abundance of the corresponding lipid species. There are two pieces of software that turn this spectrum into a lipidomics dataset: LipidSearch and LipidXplorer.
-LipidCruncher is a web app that allows the user to perform lipidomics analysis on the LipidSearch and LipidXplorer datasets. 
+This output is in the form of a spectrum in which the peaks represent an identified lipid species and the area underneath each peak represents 
+the relative abundance of the corresponding lipid species. 
+We use two pieces of software that turn this spectrum into a lipidomics dataset: LipidSearch and LipidXplorer. 
+We have built LipidCruncher which is a web-based tool that allows the biologists to perform lipidomics analysis on 
+their LipidSearch and LipidXplorer datasets.  
 
 """)
 
