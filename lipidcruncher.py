c6a141e3d104ef308a4cf3492202c3898c49f4cf
Farese & Walther Lab <86812170+FW-lab@users.noreply.github.com>
2022-11-20 16:06:10 -0500

    Update lipidcruncher.py

--- lipidcruncher.py
+++ lipidcruncher.py
@@ -6,26 +6,14 @@ from apps import lipidsearch4, lipidsearch5, lipidxplorer # import modules
 app = MultiApp()
 
 st.markdown("""
-# LipidCruncher: a web application for performing general lipidomics 
-One of our missions at Farese/Walther lab is making transformative discoveries in lipid metabolism. 
-We want to understand how our bodies store and channel energy. We are hypothesis driven. 
-A typical study in our lab is as follows: the lab biologists come up with a hypothesis (driven by deep expertise and intuition built over many years),
-then they design/run an experiment to test the hypothesis. 
-They usually have a control group where nothing is changed and one or more experimental groups where something is changed 
-(i.g. a certain gene is knocked out or is over expressed). They prepare multiple replicates for each group and run them though mass spectrometer. 
-One of the things that they are interested in is whether there is a significant difference in the lipid profile between 
-the control group and the experimental group(s).
-
-
-In the context of lipidomics, the output of mass spectrometry is the relative abundance of the lipid species that make up the sample under study. 
-This output is in the form of a spectrum in which the peaks represent an identified lipid species and the area underneath each peak represents 
-the relative abundance of the corresponding lipid species. 
-We use two pieces of software that turn this spectrum into a lipidomics dataset: LipidSearch and LipidXplorer. 
-We have built LipidCruncher which is a web-based tool that allows the biologists to perform lipidomics analysis on 
-their LipidSearch and LipidXplorer datasets.  
-
-The following map shows the lipidomics workflow in our lab:
-
+# LipidCruncher: a web application for processing, visualizing, and analyzing lipidomics data
+The advancements in biomedical engineering over the past decade has enabled the collection of lipidomics data at a scale that was not possible before. 
+However, in the absence of a lipid analytics tool, biologists are forced to analyze their data manually using tools such as Excel. 
+That is a tedious process with a high possibility of error. 
+We present LipidCruncher, a web-based tool for processing, visualizing, and analyzing lipidomics data from LipidSearch and LipidXplorer datasets. 
+LipidCruncher comes with two sub-modules. The first submodule allows the biologists to clean, filter, and explore their data, run quality checks on it, 
+and detect anomalies. The second submodule provides more in-depth analysis such as volcano plots for hypothesis testing, 
+saturation profile analysis and lipidomics pathway visualization.  
 
 """)
 
